a7bace9ad379e476dfa7265f904759d2abb8be88
Martin Doktár <martin@xtreme.se>
2010-05-11 10:31:18 +0200

    Add basic helper methods and convert layout to HAML
    * Change default layout to use HAML instead of ERB
    * Add helpers for including javascripts and stylesheets in the correct content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  
+  def javascript(*script)
+    content_for(:javascript) do 
+      javascript_include_tag(*script)
+    end
+  end
+  
+  def stylesheet(*stylesheet)
+    content_for(:stylesheet) do
+      stylesheet_link_tag(*stylesheet)
+    end
+  end
+  
 end
